50748a524655236ec8f6240a0bfa01918b7830b1
1Sebi <sebastien.pnntr@eduge.ch>
2023-03-06 15:49:10 +0100

    Etape 9

--- app/src/main/java/com/example/busschedule/viewmodels/busScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/busScheduleViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDAO
+import kotlinx.coroutines.flow.Flow
 
 class BusScheduleViewModel(private val scheduleDao : ScheduleDAO): ViewModel() {
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
 class BusScheduleViewModelFactory(
     private val scheduleDao: ScheduleDAO
